6434b21fe3cb4623d8eeb7ed3a2b3c3a02bdfc31
Rob Ratcliffe <rlr@tcliffe.dev>
2023-09-09 19:53:15 -0400

    pg 5, logic failure for multiplication test

---
+++ tdd_multi_currency_money/dollar.py
@@ -0,0 +1,8 @@
+class Dollar:
+    amount = None
+
+    def __init__(self, amount):
+        pass
+
+    def times(self, multiplier):
+        pass
--- tests/test_tests.py
+++ tests/test_tests.py
@@ -1,3 +1,5 @@
+from tdd_multi_currency_money.dollar import Dollar
+
 def test_multiplication():
     five = Dollar(5)
     five.times(2)

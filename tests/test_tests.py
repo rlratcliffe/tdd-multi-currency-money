2160b3fbb826fb95a2fff2b081eb11954e7bbc47
Rob Ratcliffe <rlr@tcliffe.dev>
2023-09-10 08:42:01 -0400

    pg 20, start of making amount private in the python way
    private isn't an available descriptor in python, so added extra test not in book to check that the upcoming refactor will do what is expected

--- tests/test_tests.py
+++ tests/test_tests.py
@@ -1,12 +1,17 @@
+import pytest
 from tdd_multi_currency_money.dollar import Dollar
 
 def test_multiplication():
     five = Dollar(5)
-    product = five.times(2)
-    assert 10 == product.amount
-    product = five.times(3)
-    assert 15 == product.amount
+    assert Dollar(10) == five.times(2)
+    assert Dollar(15) == five.times(3)
 
 def test_equality():
     assert Dollar(5) == Dollar(5) 
     assert Dollar(5) != Dollar(6)
+
+def test_cannot_change_amount():
+    with pytest.raises(AttributeError):
+        five = Dollar(5)
+        five.amount = 6
+        assert five.amount == 6

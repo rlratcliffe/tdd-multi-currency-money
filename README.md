34f6246ed797ed85599e23b082baa8af3640472b
Rob Ratcliffe <rlr@tcliffe.dev>
2023-09-10 08:49:52 -0400

    pg 20, set amount to "private" via no setter

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Part 1 of 'Test-Driven Devlopment by Example' by Kent Beck
 
 - $5 + 10 CHF = $10 if rate is 2:1
 - ~~$5 * 2 = $10~~
-- **Make "amount" private**
+- ~~Make "amount" private~~
 - ~~Dollar side-effects?~~
 - Money rounding
 - ~~equals()~~
--- tdd_multi_currency_money/dollar.py
+++ tdd_multi_currency_money/dollar.py
@@ -1,15 +1,17 @@
 import logging
 
 class Dollar:
-    amount = None
+    @property
+    def amount(self):
+        return self._amount
 
     def __init__(self, amount):
-        self.amount = amount
+        self._amount = amount
 
     def times(self, multiplier):
-        return Dollar(self.amount * multiplier)
+        return Dollar(self._amount * multiplier)
 
     def __eq__(self, dollar):
         # not sure how to cast here, dollar isn't type safe
-        logging.debug("Calculating: " + str(self.amount) + " == " + str(dollar.amount))
-        return self.amount == dollar.amount
+        logging.debug("Calculating: " + str(self._amount) + " == " + str(dollar.amount))
+        return self._amount == dollar.amount
